9d246b09fb154d02f731cf50a4c5d2e8ee750a63
Chase Smith <chasesmithdeveloper@gmail.com>
2018-06-16 23:38:16 +0000

    Add documentation to html_components

--- include/html_components.h
+++ include/html_components.h
@@ -1,12 +1,33 @@
 #ifndef HTML_COMPONENTS_STRUCT_INCLUDE
 #define HTML_COMPONENTS_STRUCT_INCLUDE
 #include "dobjects.h"
+
+// html_components_struct holds the contents of the files defined in the
+// components/ directory of a site.
 typedef struct html_components_struct {
+	// The HTML header (with opening <html> and <head> tags)
 	dstring_struct header;
+
+	// The page footer (likely has site navigation links)
 	dstring_struct footer;
+
+	// The HTML trailer (with closing </html> tag)
 	dstring_struct trailer;
 } html_components_struct;
-void html_components_free(html_components_struct*);
-void html_components_init(html_components_struct*);
-html_components_struct* html_components_load(html_components_struct*, dstring_struct*);
+
+// ==================================
+// = html_components_struct functions
+// ==================================
+
+// Cleans up the resources used by html_components; does not free
+// the pointer itself.
+void html_components_free(html_components_struct* html_components);
+
+// Initializes the specified html_components object.
+void html_components_init(html_components_struct* html_components);
+
+// Loads the component files in components_dir into html_components.
+// Returns NULL on error.
+html_components_struct* html_components_load(html_components_struct* html_components, dstring_struct* components_dir);
+
 #endif
