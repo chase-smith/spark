05bd8cebe342407794d895d63cc454dbef7adf2b
Chase Smith <chasesmithdeveloper@gmail.com>
2018-06-17 00:59:05 +0000

    Add documentation to site_generator and don't export internal functions

--- include/site_generator.h
+++ include/site_generator.h
@@ -7,15 +7,54 @@
 #include "site_configuration.h"
 #include "site_loader.h"
 
-int remove_old_tag_files(site_content_struct*, theme_struct*);
-int generate_tags(site_content_struct*);
-int generate_index_page(site_content_struct*, misc_page_struct*);
-int generate_misc_pages(site_content_struct*);
-int generate_posts(site_content_struct*);
-int make_series_dir(series_struct*, theme_struct*);
-int generate_series(site_content_struct*);
-int generate_sitemap(site_content_struct*);
-int write_rss_file_if_different(dstring_struct*, const char*, int*);
-int generate_main_rss(configuration_struct*, site_content_struct*);
-int generate_site(configuration_struct*);
+// The functions in site_generator are responsible for taking the site_content
+// and creating the HTML files for the site.
+
+// ==========================
+// = site_generator functions
+// ==========================
+
+// -----------------------------------------------------------
+// - Functions for generating specific parts of the site.
+// - It is unlikely that you will need to call these directly,
+// - instead look at the entry functions farther down.
+// -----------------------------------------------------------
+
+// Generates a page for each tag, as well as the tag listing page.
+// Returns 0 on error.
+int generate_tags(site_content_struct* site_content);
+
+// Generates all misc_pages on the site, including the index page (home page).
+// Returns 0 on error.
+int generate_misc_pages(site_content_struct* site_content);
+
+// Generates all posts on the site.
+// Returns 0 on error.
+int generate_posts(site_content_struct* site_content);
+
+// Generates each series landing page, as well as the page which lists
+// all series.
+// Returns 0 on error.
+int generate_series(site_content_struct* site_content);
+
+// Generates an HTML page with links to every (publishable) post.
+// Note, this function may be deprecated or changed significantly
+// in the future.
+// Returns 0 on error.
+int generate_sitemap(site_content_struct* site_content);
+
+// Generates the main RSS feed file for the site.
+// Returns 0 on error.
+int generate_main_rss(configuration_struct* configuration, site_content_struct* site_content);
+
+// --------------------------------------------------
+// - Entry functions; these generate the entire site.
+// --------------------------------------------------
+
+// Loads and generates the entire site.
+// Note, this function is going to change significantly in the future.
+// A better API will be created for generating sites.
+// Returns 0 on error.
+int generate_site(configuration_struct* configuration);
+
 #endif
